a05e0873ecaba27ea86d346197ec6887e07b60fc
Mahdi Mahjoobi <mahdi.m@email.com>
2020-06-12 17:07:03 +0430

    Initial Commit

---
+++ CardDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardDetection
+//
+//  Created by Mahdi Mahjoobi on 6/12/20.
+//  Copyright © 2020 Mahdi Mahjoobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
